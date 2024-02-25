e8e4a804f63f54bcf3b3eeebec3adf7a4937cbed
Valacor <valyrious.informatics@gmail.com>
2024-02-25 15:53:42 +0100

    Implement first test cases

--- QuickNote.Core.IntegrationTests/StorageContextShould.cs
+++ QuickNote.Core.IntegrationTests/StorageContextShould.cs
@@ -1,7 +1,58 @@
+using QuickNote.Core.IntegrationTests.Helper;
+using QuickNote.Core.Storage;
+using Xunit.Sdk;
 
 namespace QuickNote.Core.IntegrationTests;
 
 public class StorageContextShould {
 
-    
+    [Fact]
+    public async Task ReturnsNoneBecauseNoTodosExist() {
+
+        TestDirectoryBuilder builder = new();
+
+        string basepath = builder.Build();
+
+        MdReaderBuilder rb = new MdReaderBuilder()
+            .WithCheck()
+            .WithName();
+
+        MdWriterBuilder wb = new MdWriterBuilder()
+            .WithCheck()
+            .WithName();
+
+        StorageContext context = new StorageContext(basepath, rb, wb);
+
+        IEnumerable<Todo> result = context
+            .GetSingleAsync<Todo>(DateTime.Now)
+            .ToBlockingEnumerable();
+
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task ReturnsNoneBecauseNoTodosInExistingFile()
+    {
+        TestDirectoryBuilder builder = new();
+
+        string basepath = builder
+            .WithTodoFile(DateTime.Now.ToString("yyyy-MM-dd"))
+            .Build();
+
+        MdReaderBuilder rb = new MdReaderBuilder()
+            .WithCheck()
+            .WithName();
+
+        MdWriterBuilder wr = new MdWriterBuilder()
+            .WithCheck()
+            .WithName();
+
+        StorageContext context = new StorageContext(basepath, rb, wr);
+
+        IEnumerable<Todo> result = context
+            .GetSingleAsync<Todo>(DateTime.Now)
+            .ToBlockingEnumerable();
+
+        Assert.Empty(result);
+    }
 }
