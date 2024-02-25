8ae2eb7efc184b6444159d66a29b5c348c925973
Valacor <valyrious.informatics@gmail.com>
2024-02-25 14:37:46 +0100

    Create a builder for test directories

--- QuickNote.Core.IntegrationTests/Helper/TestDirectoryBuilder.cs
+++ QuickNote.Core.IntegrationTests/Helper/TestDirectoryBuilder.cs
@@ -1,3 +1,33 @@
+using System;
+using System.IO;
 
 namespace QuickNote.Core.IntegrationTests.Helper;
 
+internal class TestDirectoryBuilder {
+
+    private HashSet<string> TodoFiles { get; set; }
+
+    public string Basepath => Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid().ToString());
+
+    public TestDirectoryBuilder() {
+        TodoFiles = new HashSet<string>();
+    }
+
+    public TestDirectoryBuilder WithTodoFile(string name) {
+        if (string.IsNullOrWhiteSpace(name)) {
+            throw new ArgumentNullException(nameof(name));
+        }
+
+        TodoFiles.Add(name);
+        return this;
+    }
+
+    public string Build() {
+        foreach (string file in TodoFiles) {
+            string filepath = Path.Combine(Basepath, $"{file}.md");
+            File.Create(filepath);
+        }
+
+        return Basepath;
+    }   
+}
