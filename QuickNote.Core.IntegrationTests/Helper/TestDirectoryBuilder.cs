0858329afc815c6962cf5436dc01b40714965c25
Valacor <valyrious.informatics@gmail.com>
2024-02-25 15:53:07 +0100

    Update functionality of the directory builder

--- QuickNote.Core.IntegrationTests/Helper/TestDirectoryBuilder.cs
+++ QuickNote.Core.IntegrationTests/Helper/TestDirectoryBuilder.cs
@@ -2,29 +2,33 @@ namespace QuickNote.Core.IntegrationTests.Helper;
 
 internal class TestDirectoryBuilder {
 
-    private HashSet<string> TodoFiles { get; set; }
+    private Dictionary<string, Func<string>?> TodoFiles { get; set; }
 
     public string Basepath { get; init; } 
 
     public TestDirectoryBuilder() {
-        TodoFiles = new HashSet<string>();
+        TodoFiles = new Dictionary<string, Func<string>>();
         Basepath = Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid().ToString());
     }
 
-    public TestDirectoryBuilder WithTodoFile(string name) {
+    public TestDirectoryBuilder WithTodoFile(string name, Func<string>? contentProvider=null) {
         if (string.IsNullOrWhiteSpace(name)) {
             throw new ArgumentNullException(nameof(name));
         }
 
-        TodoFiles.Add(name);
+        TodoFiles.Add(name, contentProvider);
         return this;
     }
 
+    public TestDirectoryBuilder WithDateTimeFile(DateTime datetime, Func<string>? contentProvider=null) => 
+        WithTodoFile(datetime.ToString("yyyy-MM-dd"), contentProvider);
+
+
     public string Build() {
         Directory.CreateDirectory(Basepath);
-        foreach (string file in TodoFiles) {
-            string filepath = Path.Combine(Basepath, $"{file}.md");
-            File.Create(filepath);
+        foreach (KeyValuePair<string, Func<string>> file in TodoFiles) {
+            string filepath = Path.Combine(Basepath, $"{file.Key}.md");
+            File.WriteAllText(filepath, file.Value?.Invoke());
         }
 
         return Basepath;
