d6173dceac6b7cb1e78b7053d360038e38c2e46f
Valacor <valyrious.informatics@gmail.com>
2024-02-25 23:39:56 +0100

    Start writing markdown writer tests

---
+++ QuickNote.Core.IntegrationTests/MarkdownWriterTests.cs
@@ -0,0 +1,23 @@
+﻿using QuickNote.Core.Storage;
+using System.Text.Json.Serialization;
+
+namespace QuickNote.Core.IntegrationTests;
+
+public class MarkdownWriterTests
+{
+    [Fact]
+    public async Task WritesSingleTodo()
+    {
+        MarkdownWriter writer = new MdWriterBuilder()
+            .WithCheck()
+            .WithName()
+            .Build();
+
+        List<MarkdownNode> node = new List<MarkdownNode>() { new MarkdownNode(MdSyntax.Check, true.ToString()), new MarkdownNode(MdSyntax.Name, "Todo1") };
+
+
+        string result = writer.Write(node);
+
+        Assert.Equal("[X] {Todo1}", result);
+    }
+}
